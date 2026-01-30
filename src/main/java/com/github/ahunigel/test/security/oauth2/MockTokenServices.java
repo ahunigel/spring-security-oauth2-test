3239f191622f3b836b7242f961f1ea7538ef04f6
Nigel Zheng <nigel.zheng@copeland.com>
2026-01-30 10:32:28 +0800

    correct more import

--- src/main/java/com/github/ahunigel/test/security/oauth2/MockTokenServices.java
+++ src/main/java/com/github/ahunigel/test/security/oauth2/MockTokenServices.java
@@ -1,8 +1,8 @@
 package com.github.ahunigel.test.security.oauth2;
 
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockReset;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.test.context.bean.override.mockito.MockReset;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +11,6 @@ import java.lang.annotation.RetentionPolicy;
  * Created by Nigel Zheng on 8/7/2018.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@MockBean(value = {ResourceServerTokenServices.class}, reset = MockReset.AFTER)
+@MockitoBean(types = {ResourceServerTokenServices.class}, reset = MockReset.AFTER)
 public @interface MockTokenServices {
 }
